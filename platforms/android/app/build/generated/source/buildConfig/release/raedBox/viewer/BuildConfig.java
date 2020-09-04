29993f0cd9a6fdc1bc67dfa45402e1abdc066dd3
stefHin <stefan.hinterberger@outlook.at>
2020-09-04 21:14:24 +0200

    removed broken zoom plugin, added loading icon, optimized viewpoint

--- platforms/android/app/build/generated/source/buildConfig/release/raedBox/viewer/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/raedBox/viewer/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "raedBox.viewer";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10004;
-  public static final String VERSION_NAME = "1.0.4";
+  public static final int VERSION_CODE = 10006;
+  public static final String VERSION_NAME = "1.0.6";
 }
--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -1,19 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cordova-plugin-zoom-control-2.ZoomControl",
-    "file": "plugins/cordova-plugin-zoom-control-2/www/plugins.ZoomControl.js",
-    "pluginId": "cordova-plugin-zoom-control-2",
-    "clobbers": [
-      "cordova.plugins.ZoomControl"
-    ]
-  }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-zoom-control-2": "0.4.2"
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
